9b345ebabc5f498a40aa2192336d7abf5d5c6e1c
David Kerr <kerrky@gmail.com>
2024-03-14 11:25:35 -0400

    Complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import ipdb
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +13,29 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+  
+    def __init__(self, name='Bill', breed="Mastiff"):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name_param):
+        if(type(name_param) is str) and 1 <= len(name_param) <= 25:
+            self._name = name_param
+        else: 
+           print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+    @breed.setter
+    def breed(self, breed_param):
+        if(breed_param in APPROVED_BREEDS):
+            self._breed = breed_param
+        else:
+            print("Breed must be in list of approved breeds.")
+        
+    
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='Bob', job="Admin"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name_param):
+        if(isinstance(name_param, str)) and 1 <= len(name_param) <= 25:
+            self._name = name_param.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    @job.setter
+    def job(self, job_param):
+        if(job_param in APPROVED_JOBS):
+            self._job = job_param
+        else:
+            print( "Job must be in list of approved jobs.")
+
+    
